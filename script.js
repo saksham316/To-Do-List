8aa042b324b5726601a5bea91f8d74587426390a
Saksham Kothari <95004353+FantomDX316@users.noreply.github.com>
2022-03-26 21:37:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+var button_add = document.getElementById("add");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TO DO LIST</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+function elementcreating(){
+    var li = document.createElement("li");
+    var del = document.createElement("button");
+    li.setAttribute('class','list_items');
+    del.setAttribute('class','del_items');
+    li.appendChild(document.createTextNode(input.value));
+    del.textContent = "delete";
+    ul.appendChild(li);
+    li.appendChild(del);
+    input.value="";
+    del.addEventListener("click",function(event){
+        var item = event.target.del;
+        var del_par = del.parentNode;
+        var del_par_par = del_par.parentNode;
+        del_par_par.removeChild(del_par);
+    })
 
-<body>
-    <h1>TO DO LIST :-</h1>
-    <p class="para_top">List the things you wanna do</p>
-    <strong>
-        <p>Enter your items below</p>
-    </strong>
-    <div class="centerr">
-        <input id="userinput" placeholder="Enter your item" type="text">
-    </div>
-    <div class="centerr">
-        <button id="add">Add</button>
-    </div>
-    <h4>Note! Click the list item to mark it with a line</h4>
-    <h2>Below is the list of items :-</h2>
-    <ul>
-        <li id="li_first"></li>
-    </ul>
-    <script src="script.js"></script>
-</body>
+    li.addEventListener("click",function(){
+        li.style.textDecoration = "line-through";
+        li.style.textDecorationColor = "black";
+    })
 
-</html>
+}
+button_add.addEventListener("click",function(){
+    if(input.value.length>0){
+        elementcreating();
+    }
+});
+input.addEventListener("keypress",function(event){
+    if(input.value.length>0 && event.which==13){
+        elementcreating();
+    }
+});
+
+
+
+
